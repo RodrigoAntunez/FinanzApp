bcedcb0fde36d0744c65d2b45344614e908c3620
RodrigoAntunez <rodrigoagustin.antunez@estudiantes.unahur.edu.ar>
2025-05-19 20:06:45 -0300

    fix: Permitir acceso a rutas de autenticación en middleware

--- middleware.ts
+++ middleware.ts
@@ -17,8 +17,14 @@ export default withAuth(
 
 export const config = {
   matcher: [
-    "/",
-    "/api/:path*",
-    "/((?!login|api/auth).*)"
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api/auth (auth API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - login (login page)
+     */
+    "/((?!api/auth|_next/static|_next/image|favicon.ico|login).*)",
   ]
 } 
